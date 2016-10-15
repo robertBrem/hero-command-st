3844c847c0224abd7467e7ac43b227d1f8a3030e
robertBrem <brem_robert@hotmail.com>
2016-10-15 17:07:26 +0200

    chmod 750 to scripts

--- run.js
+++ run.js
@@ -2,6 +2,7 @@
 
 var containerName = "hero-command";
 var startHeroCommand = "docker run -d -p 8282:8080 --name " + containerName + " robertbrem/heros-command:" + $ENV.VERSION;
+
 $EXEC(startHeroCommand);
 print($OUT);
 print($ERR);
--- stop.js
+++ stop.js
@@ -1,10 +1,12 @@
 #!/usr/bin/jjs -fv
 
 var containerName = "hero-command";
+
 var stopHeroCommand = "docker stop " + containerName;
 $EXEC(stopHeroCommand);
 print($OUT);
 print($ERR);
+
 var rmHeroCommand = "docker rm " + containerName;
 $EXEC(rmHeroCommand);
 print($OUT);
