5ee24e0782b42944092da59b168e552df6c8bc81
robertBrem <brem_robert@hotmail.com>
2016-10-15 17:04:19 +0200

    added stop and run script

---
+++ run.js
@@ -0,0 +1,7 @@
+#!/usr/bin/jjs -fv
+
+var containerName = "hero-command";
+var startHeroCommand = "docker run -d -p 8282:8080 --name " + containerName + " robertbrem/heros-command:" + $ENV.VERSION;
+$EXEC(startHeroCommand);
+print($OUT);
+print($ERR);
---
+++ stop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/jjs -fv
+
+var containerName = "hero-command";
+var stopHeroCommand = "docker stop " + containerName;
+$EXEC(stopHeroCommand);
+print($OUT);
+print($ERR);
+var rmHeroCommand = "docker rm " + containerName;
+$EXEC(rmHeroCommand);
+print($OUT);
+print($ERR);
