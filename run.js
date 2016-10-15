4aa6a272022e0d40660fa26ff478403eef0ed84c
robertBrem <brem_robert@hotmail.com>
2016-10-15 18:42:21 +0200

    use kubectl

--- run.js
+++ run.js
@@ -1,14 +1,57 @@
 #!/usr/bin/jjs -fv
 
 var containerName = "hero-command";
-var startHeroCommand = "docker run -d -p 8282:8080 --name " + containerName + " robertbrem/heros-command:" + $ENV.VERSION;
 
+var FileWriter = Java.type("java.io.FileWriter");
+var deploymentPath = containerName + "-deployment.yml";
+var fw = new FileWriter(deploymentPath);
+fw.write("apiVersion: extensions/v1beta1\n");
+fw.write("kind: Deployment\n");
+fw.write("metadata:\n");
+fw.write("  name: hero-command-test\n");
+fw.write("spec:\n");
+fw.write("  replicas: 1\n");
+fw.write("  template:\n");
+fw.write("    metadata:\n");
+fw.write("      labels:\n");
+fw.write("        name: hero-command-test\n");
+fw.write("    spec:\n");
+fw.write("      containers:\n");
+fw.write("      - name: hero-command-test\n");
+fw.write("        image: robertbrem/heros-command:" + $ENV.VERSION + "\n");
+fw.write("        ports:\n");
+fw.write("          - containerPort: 8080\n");
+fw.close();
+
+var startHeroCommand = "kubectl create -f " + deploymentPath;
 $EXEC(startHeroCommand);
 print($OUT);
 print($ERR);
 
 
-var testUrl = "curl --write-out %{http_code} --silent --output /dev/null http://localhost:8282/hero-command/resources/heros";
+var servicePath = containerName + "-service.yml";
+var fw = new FileWriter(servicePath);
+fw.write("apiVersion: v1\n");
+fw.write("kind: Service\n");
+fw.write("metadata:\n");
+fw.write("  name: hero-command-test\n");
+fw.write("  labels:\n");
+fw.write("    name: hero-command-test\n");
+fw.write("spec:\n");
+fw.write("  ports:\n");
+fw.write("  - name: main\n");
+fw.write("    port: 8282\n");
+fw.write("    targetPort: 8080\n");
+fw.write("  selector:\n");
+fw.write("    name: hero-command-test\n");
+fw.close();
+
+var startService = "kubectl create -f " + servicePath;
+$EXEC(startService);
+print($OUT);
+print($ERR);
+
+var testUrl = "curl --write-out %{http_code} --silent --output /dev/null http://hero-command-test:8282/hero-command/resources/heros";
 $EXEC(testUrl);
 while ($OUT != "200") {
     $EXEC("sleep 1");
